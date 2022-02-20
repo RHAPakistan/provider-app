c26aa5b36474d87bba91ff6764f714253eb32eb1
LastWalkerB <jawadmustafa571@gmail.com>
2022-02-20 14:46:58 +0500

    dashboard modified

--- screens/dashboard.js
+++ screens/dashboard.js
@@ -1,16 +1,49 @@
 import React from "react";
 import { Component } from "react";
-import { StyleSheet, Text, View, Image, Button, SafeAreaView, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, PermissionsAndroid,SafeAreaView, TouchableOpacity} from 'react-native';
 import { styles } from "./styles";
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from "./home";
 import { NavigationContainer } from "@react-navigation/native"; 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import Geolocation from 'react-native-geolocation-service';
 
 export default function Dashboard({navigation}) {
   
-  function onClick(){
+  async function onClick(){
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "Cool Photo App Camera Permission",
+          message:
+            "Cool Photo App needs access to your camera " +
+            "so you can take awesome pictures.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera");
+        const loc = await Geolocation.getCurrentPosition(
+          (position) => {
+            console.log("abcd");
+            console.log(position);
+          },
+          (error) => {
+            console.log("aaaaaaa");
+            // See error code charts below.
+            console.log(error.code, error.message);
+          },
+          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+      );
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }        
     navigation.navigate('firststep')
   }
   function onClickContact(){
